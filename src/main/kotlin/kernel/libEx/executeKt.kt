2db631d7743ab669dbaf384450c3b295298eb9e9
Adam Mathlay <mathlayadam@gmail.com>
2025-01-30 09:15:14 +0200

    Fixed the VM KLib Issue all registers

--- src/main/kotlin/kernel/libEx/executeKt.kt
+++ src/main/kotlin/kernel/libEx/executeKt.kt
@@ -1,7 +1,5 @@
 package kernel.libEx
 
-import data.registers.RegisterType
-import funcR
 import internals.Vm
 import kernel.ExecuteLib
 import kernel.process.KProcess
@@ -11,20 +9,11 @@ import java.io.File
 
 suspend fun ExecuteLib.executeKt(name: String) {
 	val newVm = Vm()
-	val functionRegisterReserves =
-		vm.registers.registers.filter { it.key.name.startsWith('F') }.map { (k, v) -> k to v.copy() }.toMap()
-
+	val registerReserves =
+		vm.registers.registers.map { (k, v) -> k to v.copy() }.toMap()
 	val newKp = KProcess(newVm, File("\$lib-$name"), isKlib = true)
 	os.snapShotManager.snapShotRegisters(newKp)
-
-	for (registerType in functionRegisterReserves) {
-		vm.registers.registers[registerType.key] = registerType.value
-	}
-
-
-	if (!Klib(newKp).match(name)) {
-		vm.errors.missingLibraryException(name) // Kt should be the last resort
-	}
-
+	for (registerType in registerReserves) vm.registers.registers[registerType.key] = registerType.value
+	if (!Klib(newKp).match(name)) vm.errors.missingLibraryException(name) // Kt should be the last resort
 	currentFunction = name
 }
